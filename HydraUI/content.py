d67faaa242d867b3967f2d8190bdff346c12f24b
Aditya Bharadwaj <adityabharadwaj47@gmail.com>
2024-04-10 00:53:15 +0530

    Updated Signals on content.py to work with save option

--- HydraUI/content.py
+++ HydraUI/content.py
@@ -1,54 +1,75 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QSize
+from PySide6.QtCore import QSize, Qt
 from PySide6.QtWidgets import *
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setFixedSize(QSize(350,750))
 
         # Image/Video Options
-        op = QPushButton("O")
-        imgurl = QLineEdit()
-        radioImg = QRadioButton("Image")
-        radioVideo = QRadioButton("Video")
+        self.op = QPushButton("O")
+        self.op.clicked.connect(self.getdata)
+        self.op.setFixedSize(QSize(50,50))
 
-        radio = QButtonGroup()
-        radio.addButton(radioImg)
-        radio.addButton(radioVideo)
+        self.imgurl = QLineEdit()
+        self.imgurl.setFixedSize(QSize(330,30))
+
+        self.radioImg = QRadioButton("Image")
+        self.radioVideo = QRadioButton("Video")
+
+        self.radio = QButtonGroup()
+        self.radio.addButton(self.radioImg)
+        self.radio.addButton(self.radioVideo)
   
         a = QWidget()
         al = QHBoxLayout()
-        al.addWidget(QLabel("Image/Video URL"))
-        al.addWidget(op)
+        al.addWidget(QLabel("Image/Video URL"), alignment= Qt.AlignLeft)
+        al.addWidget(self.op, alignment= Qt.AlignRight)
         a.setLayout(al)
+        a.setFixedSize(QSize(330,70))
 
         #Header Options
-        header = QCheckBox("Header")
-        headerdata = QLineEdit()
+        self.header = QCheckBox("Header")
+        self.headerdata = QTextEdit()
+        self.headerdata.setFixedSize(QSize(330,100))
 
         #Content Options
-        content = QCheckBox("Content")
-        contentdata = QLineEdit()
+        self.content = QCheckBox("Content")
+        self.contentdata = QTextEdit()
+        self.contentdata.setFixedSize(QSize(330,250))
 
         # Create layout
         layout = QVBoxLayout(self)
         layout.addWidget(a)
-        layout.addWidget(imgurl)
-        layout.addWidget(radioImg)
-        layout.addWidget(radioVideo)
-        layout.addWidget(header)
-        layout.addWidget(headerdata)
-        layout.addWidget(content)
-        layout.addWidget(contentdata)
+        layout.addWidget(self.imgurl)
+        layout.addWidget(self.radioImg)
+        layout.addWidget(self.radioVideo)
+        layout.addWidget(self.header)
+        layout.addWidget(self.headerdata)
+        layout.addWidget(self.content)
+        layout.addWidget(self.contentdata)
 
         # Set layout
         self.setLayout(layout)
+        self.show()
+
+    def getdata(self):
+        url = self.imgurl.text()
+        hd = self.headerdata.toPlainText()
+        cd = self.contentdata.toPlainText()
+        h = self.header.isChecked()
+        c = self.content.isChecked()
+        iv = self.radio.checkedButton().text()
+        print(f"Header: {h} Checked: {c} Img/Vid: {iv}")
+        print(f"""Url: {url}
+Header Data: {hd}
+Content Data: {cd}""")
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MyWidget()
-    widget.show()
     sys.exit(app.exec())
